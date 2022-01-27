130abf63717e18d7c36f491bb88f684e0320b306
n4a3 <sanksanksank3@gmail.com>
2022-01-27 16:19:16 +0300

    Crated Wifi

--- packages/board/src/board/index.ts
+++ packages/board/src/board/index.ts
@@ -1,1 +1,11 @@
+import { initRules } from "./rules";
+// import { reloadWifi } from "./wifi";
 
+const rules = initRules();
+
+export const initBoard = () => {
+  rules.reloadRules();
+  rules.loopRules();
+
+  // reloadWifi();
+};
---
+++ packages/board/src/board/wifi.ts
@@ -0,0 +1,66 @@
+import wifi from "Wifi";
+
+const wifiPrefix = "[WiFi]";
+const apPrefix = `${wifiPrefix} [AP]`;
+const stationPrefix = `${wifiPrefix} [Station]`;
+
+const startAP = (ap: Config["wifi"]["ap"]) => {
+  let ssid = ap.ssid;
+  let options = ap;
+
+  if (!ssid) {
+    ssid = "ESP_GH_NETWORK";
+
+    options.password = "ESP_GH_12345678";
+    options.authMode = AuthMode.WPA_WPA2;
+    options.channel = 3;
+    options.hidden = 0;
+
+    console.log(apPrefix, "Not found ssid in config");
+    console.log(apPrefix, "Starting AP with default config");
+  }
+
+  wifi.startAP(ssid, options, (e: any) => {
+    if (e) {
+      console.log(apPrefix, "Error while starting AP: \n", e);
+    } else {
+      console.log(
+        apPrefix,
+        `${ssid} started with ${options.password} password!`
+      );
+    }
+  });
+};
+
+const connect = (station: Config["wifi"]["station"]) => {
+  const ssid = station.ssid;
+
+  wifi.connect(ssid, station, (e: any) => {
+    if (e) {
+      console.log(stationPrefix, "Can't connect to", ssid, e);
+    } else {
+      console.log(stationPrefix, "Connected to", ssid);
+    }
+  });
+};
+
+const startWifi = () => {
+  const config: Config = require("../static/config.json");
+
+  if (!config.wifi.station.ssid) {
+    console.log(
+      wifiPrefix,
+      "Not found config.wifi.station.ssid, starting AP..."
+    );
+    startAP(config.wifi.ap);
+  } else {
+    connect(config.wifi.station);
+  }
+};
+
+export const reloadWifi = () => {
+  // wifi.disconnect();
+  // wifi.stopAP();
+
+  startWifi();
+};
